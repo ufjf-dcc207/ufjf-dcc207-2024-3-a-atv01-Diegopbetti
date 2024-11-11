0f3ebe82883d52bb3ef03715f032bdd8a6e8252d
Diego Pereira Betti <diegopbetti.gmail.com>
2024-11-11 20:48:51 -0300

    adiciona props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
       <h1>Hello, world!</h1>
-      <Disciplina/>
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web1"/>
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web2"/>
+      <Disciplina codigo="DCC207" nome="Desenvolvimento Web Front-End"/>
     </>
   );
 }
